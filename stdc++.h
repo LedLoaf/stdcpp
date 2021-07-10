7cf2eeacb79f9af797339744b865021554380cc8
Bob <4109153+LedLoaf@users.noreply.github.com>
2021-07-10 13:46:37 -0700

    Update stdc++.h

--- stdc++.h
+++ stdc++.h
@@ -1,10 +1,10 @@
-// All C++ includes for Visual Studio 2019 that uses your set C++ Language Standard correctly. 
-// Such as (ISO C++17 Standard (/std:c++14), ISO C++17 Standard (/std:c++17), Preview - Features from the Latest C++ Working Draft (/std:c++latest))
-// Place this file in "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\include\bits" as of 7/10/2021 (create the bits folder)
-// The '14.29.30037' portion will change in the future and make sure to use the newest version, or add them to all if you use multiple versions.
-// Originally, this file wasn't reading the specific set C++ Language Standard in Visual Studio 2019.
-// Adding the MY_CPLUSPLUS defines allowed it to work correctly for me.
-// Visit https://gist.github.com/raydwaipayan/a48ef18210ee07f8b3d34b114b395b46 for the original file I used. 
+// All C++ includes for Visual Studio 2019 that uses your set C++ Language Standard correctly
+// Such as (/std:c++14), (/std:c++17), (/std:c++latest))
+// Place this file in "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\include\bits" (create the bits folder)
+// The '14.29.30037' portion is subject to change and use the newest version, or add them to all if you use multiple versions.
+// Originally, this file wasn't reading the specific set C++ Language Standard in Visual Studio 2019 for me.
+// Adding the ***MY_CPLUSPLUS*** defines allowed it to work correctly for me.
+// Visit https://gist.github.com/raydwaipayan/a48ef18210ee07f8b3d34b114b395b46 (for the original file I used)
 
 #ifndef MY_CPLUSPLUS 
 #if defined(_MSVC_LANG) && !defined(__clang__)
